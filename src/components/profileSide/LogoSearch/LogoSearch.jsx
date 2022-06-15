02a97191a02e7a6193549c8bfc74e923fc5366b9
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-06-15 15:11:02 +0200

    added basic folders created by react, added components and pages folder

---
+++ src/components/profileSide/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {UilSearch} from '@iconscout/react-unicons' 
+import Logo from "../../../img/logo.png"
+import './LogoSearch.css'
+export default function LogoSearch() {
+  return (
+    <div className="LogoSearch">
+        <img src={Logo} alt="" />
+        <div className="Search">
+            <input type="text" placeholder="#Explore"/>
+            <div className="s-icon">
+                <UilSearch/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProfileSide from '../../components/profileSide/ProfileSide'
+import './Home.css'
+function Home() {
+  return (
+    <div className="Home">
+        <ProfileSide/>
+        <div className="postSide">Posts</div>
+        <div className="RightSide">RightSide</div>
+    </div>
+  )
+}
+
+export default Home
