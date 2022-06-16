8d5fbd742dcc57fb5895591be461148e950af81e
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-06-16 19:17:58 +0200

    added static data for testing components, added trendCard component

---
+++ src/Data/TrendData.js
@@ -0,0 +1,30 @@
+export const TrendData = [
+    {
+        name: "Minions",
+        shares: 97,
+    },
+    {
+        name: "Star Wars",
+        shares: 98,
+    },
+    {
+        name: "Star Trek",
+        shares: 99,
+    },
+    {
+        name: "Star Wars: The Force Awakens",
+        shares: 100,
+    },
+    {
+        name: "Elon Musk",
+        shares: 101,
+    },
+    {
+        name: "Tesla",
+        shares: 102,
+    },
+    {
+        name: "SpaceX",
+        shares: 103,
+    }
+]
