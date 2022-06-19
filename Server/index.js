947014fcecae46158af869eb89bde5ecc0b766a8
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-06-19 17:46:52 +0200

    added basic server side part

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => app.listen(process.env.PORT, () => 
+    console.log("Listening..."))
+  )
+  .catch((error) => console.log(error));
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -5,6 +5,7 @@ import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
+import ShareModal from "../ShareModal/ShareModal";
 const RightSide = () => {
   const [modalOpened, setModalOpened] = useState(false);
   return (
